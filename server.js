e13bb8ce4fa9d7da3cb41cbdfcd2a2b6f644997e
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-04-29 22:17:25 +0900

    ..

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ app.listen(8080, function(){
     console.log("listening on 8080")
 })
 
-app.get('/pet', function(요청, 응답){
-    응답.send("펫용품")
-})
-
 app.get('/', function(요청, 응답){
     응답.sendFile(__dirname+'/index.html')
+})
+
+app.get('/write', function(요청, 응답){
+    응답.sendFile(__dirname+'/write.html')
 })
