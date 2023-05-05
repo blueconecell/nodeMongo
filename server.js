0d889e852eadb2b2ce0656937c12791fc886eaf4
blueconecell <95961799+blueconecell@users.noreply.github.com>
2023-05-05 22:02:02 +0900

    not work

--- server.js
+++ server.js
@@ -19,10 +19,13 @@ MongoClient.connect(
     });
 
     app.listen(8080, function () {
-      console.log("listening on 8080");
+      console.log("1. listening on 8080");
     });
   }
 );
+app.listen(8080, function () {
+  console.log("2. listening on 8080");
+});
 app.get("/", function (요청, 응답) {
   응답.sendFile(__dirname + "/index.html");
 });
@@ -34,4 +37,4 @@ app.post("/add", function (요청, 응답) {
   console.log(요청.body.date);
   console.log(요청.body.title);
 });
-//
+
