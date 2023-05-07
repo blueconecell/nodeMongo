4afc00891314b78c1f27dda676b7ebdaef4e139c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-05-07 21:54:01 +0900

    not work nodemon

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const MongoClient = require("mongodb").MongoClient;
 const express = require("express");
 const app = express();
-var db;
+app.use(express.urlencoded({ extended: true }));
+// var db;
 
-MongoClient.connect(
-  "mongodb+srv://kimjeongyeon113:smart123@cluster0.vr7efrs.mongodb.net/?retryWrites=true&w=majority",
-  { useUnifiedTopology: true },
-  function (에러, client) {
-    console.log("시작됨??")
-    // 연결되면 할일
-    if (에러) return console.log(에러);
+// MongoClient.connect(
+//   "mongodb+srv://kimjeongyeon113:smart123@cluster0.vr7efrs.mongodb.net/?retryWrites=true&w=majority",
+//   { useUnifiedTopology: true },
+//   function (에러, client) {
+//     console.log("시작됨??");
+//     // 연결되면 할일
+//     if (에러) return console.log(에러);
 
-    db = client.db("todoApp");
+//     db = client.db("todoApp");
 
-    //데이터 저장할 때 쓰이는 코드
-    db.collection("post").insertOne({ 이름: "kimjeongyeon", 나이: 23 }, function (에러, 결과) {
-      console.log("저장완료");
-    });
+//     //데이터 저장할 때 쓰이는 코드
+//     db.collection("post").insertOne({ 이름: "kimjeongyeon", 나이: 23 }, function (에러, 결과) {
+//       console.log("저장완료");
+//     });
 
-    app.listen(8080, function () {
-      console.log("1. listening on 8080");
-    });
-  }
-);
+//     app.listen(8080, function () {
+//       console.log("1. listening on 8080");
+//       console.log("1. listening on 8080");
+//     });
+//   }
+// );
 app.listen(8080, function () {
   console.log("2. listening on 8080");
 });
@@ -34,7 +36,6 @@ app.get("/write", function (요청, 응답) {
 });
 app.post("/add", function (요청, 응답) {
   응답.send("전송완료");
-  console.log(요청.body.date);
   console.log(요청.body.title);
+  console.log(요청.body.date);
 });
-
