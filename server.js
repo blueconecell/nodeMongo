9a07b7578ffe56fc231c6fbe351134200045497d
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-05-17 18:56:55 +0900

    login ~3.5

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const methodOverride = require("method-override");
-app.use(methodOverride("_method"));
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 
+// 미들웨어 3개
+// 미들웨어: 요청과 응답 중간에 app.use를 사용하여 중간에 동작시키는 것
+app.use(session({ secret: "kitchengun", resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public")); //미들웨어
@@ -95,4 +104,50 @@ app.put("/edit", function (요청, 응답) {
     console.log("수정완료");
     응답.redirect("/list");
   });
+});
+
+// 로그인기능
+app.get("/login", function (요청, 응답) {
+  응답.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/fail",
+  }),
+  function (요청, 응답) {
+    응답.redirect("/");
+  }
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "id",
+      passwordField: "pw",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (입력한아이디, 입력한비번, done) {
+      console.log(입력한아이디, 입력한비번);
+      db.collection("login").findOne({ id: 입력한아이디 }, function (에러, 결과) {
+        if (에러) return done(에러);
+
+        if (!결과) return done(null, false, { message: "존재하지않는 아이디요" });
+        if (입력한비번 == 결과.pw) {
+          return done(null, 결과);
+        } else {
+          return done(null, false, { message: "비번틀렸어요" });
+        }
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (아이디, done) {
+  done(null, {});
 });
